8623ce031727c9676f25476728c5af571232cf7b
Maxime Kestemont <m.kestemont@criteo.com>
2020-04-10 16:22:53 +0200

    Project setup + indicator bar prefab

---
+++ Assets/Scripts/Managers/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public ResourceManager resourceManager;
+
+    public void StartGame()
+    {
+        Debug.Log ("Starting the game...");
+        resourceManager.menuPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UI/IndicatorBar.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+/*
+Indicator bar, handling the UI update.
+Range between 0 and 100.
+ */
+public class IndicatorBar : MonoBehaviour {
+
+	public GameObject filling;
+	public GameObject border;
+	public GameObject text;
+
+	public string indicatorName = "DefaultName";
+	public int fillingLevel = 75;
+	
+	private RectTransform borderRect;
+	private const int MIN_VALUE = 0;
+	private const int MAX_VALUE = 100;
+
+	void Start () {
+		borderRect = border.GetComponent<RectTransform> ();
+		text.GetComponent<Text>().text = indicatorName;
+	}
+
+
+	void Update () {
+		// Update the bar rendering
+		filling.transform.localScale = new Vector3 (
+			fillingLevel / 100f * borderRect.localScale.x, 
+			filling.transform.localScale.y, 
+			filling.transform.localScale.z
+		);
+	}
+
+	/*
+	=====================
+	UpdateIndicator
+	=====================
+	Update the indicator by a value (can be negative).
+	If it goes below or above the range, TODO something happens
+	*/
+	public void UpdateIndicator(int value) {
+		fillingLevel = fillingLevel + value;
+
+		if (fillingLevel < MIN_VALUE) {
+			Debug.Log ($"Indicator {indicatorName} is too low !");
+		}
+
+		if (fillingLevel < MAX_VALUE) {
+			Debug.Log ($"Indicator {indicatorName} is too high !");
+		}
+
+	}
+}
