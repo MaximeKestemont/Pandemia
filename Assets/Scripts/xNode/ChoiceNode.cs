b5e02162ae21ecf9fe9076862af8b141dd05ee6a
MaximeKestemont <m.kestemont@criteo.com>
2020-04-23 20:38:14 +0200

    Add xNode package and skeleton

---
+++ Assets/Scripts/xNode/ChoiceNode.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+public class ChoiceNode : Node { 
+	[Input] public EventNode correspondingEvent;
+    [Output] public EventNode lockingEvent;
+    [Output] public EventNode unlockingEvent;
+    [Output] public EventNode passingEvent;
+
+	protected override void Init() {
+		base.Init();
+	}
+
+    // Return the correct value of an output port when requested
+	public override object GetValue(NodePort port) {
+		return null; // Replace this
+	}
+}
